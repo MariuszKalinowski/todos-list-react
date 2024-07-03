d9ab6dee782f59f4335127830a20b4990694accb
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-07-03 19:27:13 +0200

    added function for hidding and showing done tasks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Form from "./Form";
 import "./Form/style.css";
 import TasksList from "./TasksList";
@@ -10,9 +11,14 @@ const tasks = [
   { id: 2, content: "Zjeść kolację", done: true },
 ];
 
-const hideDone = false;
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  }
+
   return (
     <div className="container">
       <Header title={"Lista zadań"} />
@@ -21,7 +27,7 @@ function App() {
       <Section
         title="Lista zadań"
         body={<TasksList tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />} />
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />} />
     </div>
   );
 }
