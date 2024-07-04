7e0aded05c6233a55facf52267be3c6a9d6268b5
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2024-07-04 21:32:00 +0200

    changed span to div

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,11 +2,11 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone}) => (
     tasks.length > 0 && (
-        <span className="section__renderButtons">
+        <div className="section__renderButtons">
             <button onClick={toggleHideDone} className="form__renderButton"> {hideDone ? "Pokaż" : "Ukryj"} ukończone </button>
             <button className="form__renderButton" disabled={tasks.every(({ done }) => done)}
             > Ukończ wszystkie </button>
-        </span>
+        </div>
     )
 );
 
